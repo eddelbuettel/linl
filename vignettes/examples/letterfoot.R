b70e9e3423700354e19ab18e925463876c9b8689
Iñaki Úcar <iucar@fedoraproject.org>
2023-01-07 10:48:41 +0100

    demonstrate new functionality

--- vignettes/examples/letterfoot.R
+++ vignettes/examples/letterfoot.R
@@ -1,10 +1,10 @@
 # remotes::install_github("coolbutuseless/minipdf")
 library(minipdf)
-doc <- PDFDocument$new(width=800, height=80, fontname="Helvetica")
-doc$text("Less professional letterfoot", x=80, y=25, fontsize=40)
+doc <- PDFDocument$new(width=700, height=60, fontname="Helvetica")
+doc$text("Less professional letterfoot", x=0, y=25, fontsize=40)
 # doc$line(x1=80, y1=9, x2=720, y2=9)
 k <- 30
-ox <- seq(85, 715, length.out = k)
+ox <- seq(5, 635, length.out = k)
 pal <- terrain.colors(k)
 for( i in seq(along=ox)) {
     doc$circle(ox[i], 5, 5, fill=pal[i], stroke=NULL)
